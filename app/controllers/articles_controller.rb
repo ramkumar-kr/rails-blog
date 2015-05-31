80375c5a73a990195eee0969bd165c712b5a07ed
Ram Kumar K R <ramkumar.kr@urbanladder.com>
2015-05-31 16:41:27 +0530

    Added edit and update functionality

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,10 +1,41 @@
 class ArticlesController < ApplicationController
 	def new
+  	@article = Article.new
+	end
+	
+	def edit
+		@article = Article.find(params[:id])
 	end
 
-	def create
-		@article = Article.new(params.require(:article).permit(:title, :text))
-		@article.save
-		redirect_to @article
+	def index
+		@article = Article.all
 	end
+
+	def show
+		@article  = Article.find(params[:id])
+	end
+ 
+	def create
+  	@article = Article.new(article_params)
+ 
+  	if @article.save
+    	redirect_to @article
+  	else
+    	render 'new'
+  	end
+	end
+	
+	def update
+  	@article = Article.find(params[:id])
+  	if @article.update(article_params)
+    	redirect_to @article
+  	else
+    	render 'edit'
+  	end
+	end
+ 
+	private
+  def article_params
+    params.require(:article).permit(:title, :text)
+  end
 end
