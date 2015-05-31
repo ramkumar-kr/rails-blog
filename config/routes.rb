adc437fd86d2a2e0c81d7816c12715e13f4f1f93
Ram Kumar K R <ramkumar.kr@urbanladder.com>
2015-05-31 13:48:48 +0530

    Added Articles new controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  get 'welcome/index'
+  resources :articles
+	get 'welcome/index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
