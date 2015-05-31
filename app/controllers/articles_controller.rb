57451ccc28056f4fce131a5bec9aff7513962c48
Ram Kumar K R <ramkumar.kr@urbanladder.com>
2015-05-31 18:27:02 +0530

    Completed articles model and added Comment model

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -8,11 +8,17 @@ class ArticlesController < ApplicationController
 	end
 
 	def index
-		@article = Article.all
+		@articles = Article.all
 	end
 
 	def show
 		@article  = Article.find(params[:id])
+	end
+
+	def destroy
+  	@article = Article.find(params[:id])
+	  @article.destroy
+  	redirect_to articles_path
 	end
  
 	def create
