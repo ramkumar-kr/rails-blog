f37d565b15cbc99af60dbc67df18b45bb5d32e28
Ram Kumar K R <ramkumar.kr@urbanladder.com>
2015-05-31 18:41:51 +0530

    Completed comments

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,5 @@
 class Article < ActiveRecord::Base
-  validates :title, presence: true, length: { minimum: 5 }
+  has_many :comments, dependent: :destroy
+	validates :title, presence: true, length: { minimum: 5 }
 	validates :text, presence: true, length:{ minimum: 10 }
 end
